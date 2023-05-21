672209d8a3c57ccaa5aba23bc4119111066f61d6
TechSammy <mrsam03102002@gmail.com>
2023-05-21 20:03:13 +0530

    setting up chatbox and sendmessage files into chatroom

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
 
+import ChatRoom from  "./pages/ChatRoom"
+
 function App() {
   
 
   return (
     <div >
      <Navbar/>
-     <Login/>
+     {/* <Login/> */}
+     <ChatRoom/>
     </div>
   )
 }
