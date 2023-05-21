8ce15ceb91f0b4acfba0b9cf12d8d9e5cc55eb61
TechSammy <mrsam03102002@gmail.com>
2023-05-21 21:49:55 +0530

    done with styling the input and send buttons

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const SendMessage = () => {
+
+    
   return (
-    <div>SendMessage</div>
+    <div className='bg-purple-900 fixed bottom-0 w-full py-5 shadow-lg '>
+        <form className='ContainerWrap flex'>
+        <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xxl rounded-r-none" />
+        <button className="btn btn-primary rounded-l-none">Send</button>
+        </form>
+    </div>
   )
 }
 
