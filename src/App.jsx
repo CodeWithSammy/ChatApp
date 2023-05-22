3b97ced055ebde187d09a3c0e007c57c1806e47c
TechSammy <mrsam03102002@gmail.com>
2023-05-22 14:07:54 +0530

    made few changes in privateRoute to only show chat when logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
-import ChatRoom from  "./pages/ChatRoom"
-import { Route, Routes } from 'react-router-dom'
+import ChatRoom from "./pages/ChatRoom";
+import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
-  
-
   return (
-    <div >
-     <Navbar/>
-     <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/chat" element={<ChatRoom/>} />
-     </Routes>
-     
-     
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <ChatRoom />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
