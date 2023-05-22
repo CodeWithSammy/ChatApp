79b8b8d60c73843120389690f74588c85613152e
TechSammy <mrsam03102002@gmail.com>
2023-05-22 14:08:22 +0530

    setting up firebase authentication

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCG925nb0R4zXB8brteVbxglHGg0aJULy4",
+  authDomain: "letstalk-bd5d2.firebaseapp.com",
+  projectId: "letstalk-bd5d2",
+  storageBucket: "letstalk-bd5d2.appspot.com",
+  messagingSenderId: "741567959206",
+  appId: "1:741567959206:web:1b898f1959e7efbbd06467"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
