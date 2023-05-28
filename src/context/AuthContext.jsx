97c7b3b29fe3a6376495d119fa6dc43490d3d4d8
TechSammy <mrsam03102002@gmail.com>
2023-05-28 22:21:25 +0530

    working on user auth, in authcontext file

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+const {createContext, useState} = require("react");
+//create context
+const AuthContext =  createContext();
+export const AuthProvider =({children})=>{
+    const[currentUser,setCurrentUser] = useState(null);
+
+    const value ={
+        currentUser,
+        setCurrentUser
+    }
+    return(
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export const UserAuth = ()=>{
+    return useContext(AuthContext);
+}
